23aa050195d3e12a4ca401a93c322543a98693d9
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-12-28 11:17:07 +0100

    added list of emails for invitation completion

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -3,6 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
     Route.get('/users/me', 'UserController.get')
     Route.get('/rooms', 'UserController.listRooms')
+    Route.get('/users/list', 'UserController.list')
 }).middleware('auth')
 Route.post('/login', 'AuthenticationController.login')
 Route.post('/register', 'AuthenticationController.register')
